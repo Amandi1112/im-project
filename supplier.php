9225809a95930be2006a030f3d94ff9c66fc38dc
“[amandi <[amandirashini1112@gmail.com]>
2025-07-14 10:57:24 +0530

    title changed

--- supplier.php
+++ supplier.php
@@ -81,7 +81,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Supplier Registration | Beautiful Interface</title>
+    <title>Supplier Registration</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap">
     <style>
         * {
